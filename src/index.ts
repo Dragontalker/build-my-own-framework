29d395a3b3fa4d17daa6c55de94ba912a7b0b510
Richard <richard.yang.tong@gmail.com>
2021-04-25 12:09:59 -0400

    Tested set method in main app.

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Richard', age: 20});
 
+user.set({ name: 'Tong', age: 32});
+
 console.log(user.get('name'));
 console.log(user.get('age'));
