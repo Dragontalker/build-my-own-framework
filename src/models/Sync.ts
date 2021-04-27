208153a2bc1be9e8b496d380e2921b2b571efd67
Richard <richard.yang.tong@gmail.com>
2021-04-27 09:59:27 -0400

    Moved save to Sync.

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,1 +1,19 @@
-export class Sync {}
+export class Sync {
+  fetch(): void {
+    axios.get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      }
+    );
+  }
+
+  save(): void {
+    const id = this.get('id')
+    if (id) {
+      // put
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post('http://localhost:3000/users', this.data);
+    }
+  }
+}
