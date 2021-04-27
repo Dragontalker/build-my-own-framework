437dcb5ec6958c7315bb365a17868b1335d9e1f7
Richard <richard.yang.tong@gmail.com>
2021-04-27 08:01:03 -0400

    Tested another case with no id.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1});
-
-user.set({ name: 'NEW NAME' });
-user.set({ age: 9999 });
+const user = new User({ name: 'new record', age: 0});
 
 user.save();
