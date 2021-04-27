138c7cc77887b90691cc68906e27f95c6edd7363
Richard <richard.yang.tong@gmail.com>
2021-04-26 20:32:56 -0400

    Tested tigger method.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,15 @@ import { User } from './models/User';
 
 const user = new User({});
 
-user.on('change', () => {});
-user.on('change', () => {});
-user.on('notice', () => {});
+user.on('change', () => {
+  console.log('Change #1');
+});
+user.on('change', () => {
+  console.log('Change #2');
+});
+user.on('save', () => {
+  console.log('Save was triggered')
+});
 
-console.log(user);
+user.trigger('change');
+user.trigger('save');
